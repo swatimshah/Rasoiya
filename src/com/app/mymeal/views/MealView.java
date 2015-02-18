dfd634883addfa7c5111bac75fb326f77893fe19
swatimshah <swati.matwankar@gmail.com>
2015-02-18 21:23:59 +0530

    Refactored - phase 2 - comments removed

--- src/com/app/mymeal/views/MealView.java
+++ src/com/app/mymeal/views/MealView.java
@@ -24,27 +24,11 @@ public class MealView extends LinearLayout {
 	private ListView listView;
 	private Button addButton;
 	private Button clearButton;
-//	private CommonView commonView;
-//	private Context context;
-//	private MyCustomAdapter dataAdapter;
 	private String mealLabel;
-
-
 	
 	
-//	
-//	public CommonView getCommonView() {
-//		return commonView;
-//	}
-//
-//	public void setCommonView(CommonView commonView) {
-//		this.commonView = commonView;
-//	}
-//
 	public MealView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-
-//		this.context = context;
 
 		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -63,40 +47,9 @@ public class MealView extends LinearLayout {
 		Log.e("error", "mealLabel: " + mealLabel);
 		lblMeal.setText(mealLabel);
 		array.recycle();
-//		search_meal_icon.setOnClickListener(myOnClickListener);
+
 	}
 
-//	OnClickListener myOnClickListener = new OnClickListener() {
-//
-//		@Override
-//		public void onClick(View v) {
-//			// TODO Auto-generated method stub
-//			searchView.setVisibility(View.VISIBLE);
-//			addButton.setVisibility(View.VISIBLE);
-//			clearButton.setVisibility(View.VISIBLE);
-//			commonView.setMealType(mealType);
-//			commonView.setupSearchView(context);
-//			commonView.clearListView(context, dataAdapter);
-//			Log.e("error",
-//					"ConcatenatedMealItem onClick: "
-//							+ dataAdapter.getConcatAddedMealItems());
-//
-//			listView.setVisibility(View.VISIBLE);
-//
-//			commonView.getLunch().getSearch_meal_icon().setEnabled(false);
-//			commonView.getLunch().getSearch_meal_icon()
-//					.setImageResource(R.drawable.search_icon_small2_disabled);
-//			commonView.getSnacks().getSearch_meal_icon().setEnabled(false);
-//			commonView.getSnacks().getSearch_meal_icon()
-//					.setImageResource(R.drawable.search_icon_small2_disabled);
-//			commonView.getDinner().getSearch_meal_icon().setEnabled(false);
-//			commonView.getDinner().getSearch_meal_icon()
-//					.setImageResource(R.drawable.search_icon_small2_disabled);
-//
-//			setBackgroundColor(Color.rgb(255, 239, 213));
-//
-//		}
-//	};
 
 	public TextView getLblMeal() {
 		return lblMeal;
@@ -154,17 +107,6 @@ public class MealView extends LinearLayout {
 		this.clearButton = clearButton;
 	}
 
-//	public void setContext(Context context) {
-//		this.context = context;
-//	}
-
-//	public String getMealType() {
-//		return mealType;
-//	}
-//
-//	public void setMealType(String mealType) {
-//		this.mealType = mealType;
-//	}
 
 	/**
 	 * @param args
